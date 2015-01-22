aaac5bb1c0c28685b871cf0818f38141be4bc900
Leonid Kyrpychenko <kyrpychenko@qumram.ch>
2015-01-22 10:26:03 +0100

    MongoDb Chef recipe

---
+++ mongodb/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "Leonid Kyrpychenko"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
